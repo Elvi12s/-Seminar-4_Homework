205218d603931502cc6c09cbc00816461023c89e
Elvis <elvisnepresly@yandex.ru>
2023-01-13 01:04:56 +0300

    Task 34

---
+++ Csharp_HW05/Task 34/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[Num];
+int length = array.Length;
+int count=0;
+for (int i = 0; i <length; i++)
+{
+  array[i] = new Random().Next(100,1000);
+  Console.Write($"{array[i]}, ");
+  if (array[i]%2==0) count++;
+}
+Console.WriteLine();
+Console.Write($"Всего четных чисел :{count}.");
