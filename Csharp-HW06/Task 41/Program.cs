d690374271c5144356dd1825b66d6d191bf8a200
Elvis <elvisnepresly@yandex.ru>
2023-01-14 14:58:56 +0300

    Task 41

---
+++ Csharp-HW06/Task 41/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int count=0;
+void Vvodmassiva()
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(-100, 100);
+    Console.Write($"{array[i]} ");
+    if (array[i] > 0)  count++;
+  }
+}
+Vvodmassiva();
+System.Console.WriteLine();
+System.Console.WriteLine($"пользователь ввел {count} чисел больше 0");
