5eae601d541367c2492e37895c2c42a2702fa46f
Elvis <elvisnepresly@yandex.ru>
2023-01-20 12:20:07 +0300

    Task 66

---
+++ Csharp-HW09/Task 66/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int  InNum(int N, int M)
+{
+if(N<=0) N=1;
+if(M<=0) M=1;
+if(M>N) return N+InNum(M,N+1);
+else if (M<N) return M+ InNum(M+1,N); 
+else return N;
+}
+Console.WriteLine("Введите 1-ое число: ");
+int num1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(InNum(num1,num2));
